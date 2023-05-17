f936ef02c5f163e1216d0709dd3a370964faea5c
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-05-16 22:23:20 -0500

    added more, testing if env file will not show on github

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import {useState} from 'react';
 import {
   SafeAreaView,
   Text,
   View,
   TextInput,
+  Pressable,
 } from 'react-native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App';
@@ -14,12 +15,20 @@ export type NavigationProp = NativeStackNavigationProp<RootStackParamList, "Home
 
 const Home = (props: Props) => {
 
+    const [weatherData, setWeatherData] = useState()
+
+    const handleSubmit = async () => {
+        const result = await fetch("").then((data) => data.json())
+        setWeatherData(result)
+    }
+    
 
     return (
         <SafeAreaView className="bg-gray">
             <View className="bg-black/60 text-center mx-auto mt-10 p-5 rounded">
                 <Text className="text-white text-xl mx-10 text-center">Check out the weather from around the world!</Text>
-                <TextInput></TextInput>
+                <TextInput className="bg-white py-1 my-5 w-60 mx-auto"></TextInput>
+                <Pressable className="text-center flex-end" ><Text>Get Weather</Text></Pressable>
             </View>
         </SafeAreaView>
     )
